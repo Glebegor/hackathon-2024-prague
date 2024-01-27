ae8527a7d2f9528d1cbbb1702b325326cbdb1040
Glebegor <glebegor2220@gmail.com>
2024-01-27 15:45:49 +0100

    Changed controllers.

---
+++ backend/api/controller/contact_controller.go
@@ -0,0 +1,18 @@
+ package controller
+ 
+ import(
+         "net/http"
+         "github.com/gin-gonic/gin"
+ 
+ )
+ 
+ 
+ type ContactsController struct {
+ 
+ }
+ 
+ func (tc *ContactsController) Create(c *gin.Context) {}
+ func (tc *ContactsController) Delete(c *gin.Context) {}
+ func (tc *ContactsController) Update(c *gin.Context) {}
+ func (tc *ContactsController) Get(c *gin.Context) {}
+ func (tc *ContactsController) GetAll(c *gin.Context) {}
---
+++ backend/api/controller/event_controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import(
+	"net/http"
+	"github.com/gin-gonic/gin"
+
+)
+
+
+type EventController struct {
+	
+}
+
+func (tc *EventController) Create(c *gin.Context) {}
+func (tc *EventController) Delete(c *gin.Context) {}
+func (tc *EventController) Update(c *gin.Context) {}
+func (tc *EventController) Get(c *gin.Context) {}
+func (tc *EventController) GetAll(c *gin.Context) {}
