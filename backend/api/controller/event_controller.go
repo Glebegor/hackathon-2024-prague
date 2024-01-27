bf91aa921194452e118b6d15ee9887a74cdc755b
Glebegor <glebegor2220@gmail.com>
2024-01-27 18:36:11 +0100

    Added standart responses in route.

--- backend/api/controller/event_controller.go
+++ backend/api/controller/event_controller.go
@@ -1,7 +1,7 @@
 package controller
 
 import(
-	// "net/http"
+	"net/http"
 	"github.com/gin-gonic/gin"
 	"ton-event-bot/domain"
 )
@@ -11,8 +11,46 @@ type EventController struct {
 	EventUsecase domain.EventUsecase	
 }
 
-func (tc *EventController) Create(c *gin.Context) {}
-func (tc *EventController) Delete(c *gin.Context) {}
-func (tc *EventController) Update(c *gin.Context) {}
-func (tc *EventController) GetById(c *gin.Context) {}
-func (tc *EventController) GetAll(c *gin.Context) {}
+func (tc *EventController) Create(c *gin.Context) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "Ok",
+	})
+}
+func (tc *EventController) Delete(c *gin.Context) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "Ok",
+	})
+
+}
+func (tc *EventController) Update(c *gin.Context) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "Ok",
+	})
+}
+func (tc *EventController) GetById(c *gin.Context) {
+	var data domain.Event
+        
+
+	data, err := tc.EventUsecase.GetById(c)
+        if err != nil {
+                c.JSON(http.StatusInternalServerError, map[string]interface{}{
+                        "message": "Error: Cant get event.",
+                })
+                fmt.Print(err.Error())
+                return
+        }
+        c.JSON(http.StatusOK, data)
+}
+func (tc *EventController) GetAll(c *gin.Context) {
+	var data []domain.Event
+	data, err := tc.EventUsecase.GetAll(c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Error: Cant get all events.", 
+		})
+		fmt.Print(err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, data)
+}
+
