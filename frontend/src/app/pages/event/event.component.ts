2c8680233384fbd2acc6036085af1745a19b07dd
Arsen290 <arsen290@ukr.net>
2024-01-28 02:29:00 +0100

    add refresh page

--- frontend/src/app/pages/event/event.component.ts
+++ frontend/src/app/pages/event/event.component.ts
@@ -3,6 +3,7 @@ import { EventsService, ISubscribtion,  } from '../../services/events.service';
 import { TelegramService } from '../../services/telegram.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CacheService } from '../../services/cache.service';
 
 @Component({
   selector: 'app-event',
@@ -19,24 +20,26 @@ export class EventComponent implements OnInit, OnDestroy {
     private telegram:TelegramService,
     private route:ActivatedRoute,
     private router:Router,
-    
+    private cacheService: CacheService
     ){
       const id = this.route.snapshot.paramMap.get('id');
       this.subscribtion = this.subscribtions.getById(Number(id));
       this.telegram.MainButton.show();
       this.telegram.MainButton.setText("Subscribe!");
       this.goBack = this.goBack.bind(this)
-  }
-  goBack(){
-    this.router.navigate(['/'])
+      this.telegram.BackButton.show();
+   }
+  
+  ngOnInit(): void {
+    this.telegram.BackButton.onClick(this.goBack);           
   }
   ngOnDestroy(): void {
     this.telegram.BackButton.offClick(this.goBack);
+    this.cacheService.reloadPage();
   }
-  ngOnInit(): void {
-    this.telegram.BackButton.show();
-    this.telegram.BackButton.onClick(this.goBack);
-
+  goBack(){
+    this.router.navigate(['/']);
   }
+  
 
 }
--- frontend/src/app/pages/events/events.component.ts
+++ frontend/src/app/pages/events/events.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { TelegramService } from '../../services/telegram.service';
 import { CommonModule } from '@angular/common';
 import { EventsService } from '../../services/events.service';
 import { EventListComponent } from '../../components/event-list/event-list.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CacheService } from '../../services/cache.service';
 
 @Component({
   selector: 'app-events',
@@ -12,7 +13,7 @@ import { HeaderComponent } from '../../components/header/header.component';
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
-export class EventsComponent {
+export class EventsComponent{
   //inject btn
   telegram= inject(TelegramService);
   subscribtions = inject(EventsService);
@@ -21,5 +22,7 @@ export class EventsComponent {
     this.telegram.MainButton.show();
     this.telegram.BackButton.hide();
     this.telegram.MainButton.setText("Add channel");
+  
   }
+
 }
